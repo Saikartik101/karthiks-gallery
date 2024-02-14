1ffde2438b0df22fdd2da2d865737e346fddc625
Saikartik101 <73304283+Saikartik101@users.noreply.github.com>
2024-02-14 21:03:22 +0530

    Add files via upload

---
+++ src/Images.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+
+interface ImagesProps {
+  data: {
+    src: string;
+    title: string;
+    description: string;
+  }[];
+  onClick: (index: number) => void;
+}
+
+const Images: FC<ImagesProps> = (props) => {
+  const { data, onClick } = props;
+
+  const handleClickImage = (index: number) => {
+    onClick(index);
+  };
+
+  return (
+    <>
+    <h1>Karthik's Gallery React</h1>
+    <div className='images-container'>
+      {data.map((slide, index) => (
+        <div
+          onClick={() => handleClickImage(index)}
+          key={index}
+          className='image'
+        >
+          <img src={slide.src} alt={slide.description} />
+        </div>
+      ))}
+    </div>
+    </>
+  );
+};
+
+export default Images;
